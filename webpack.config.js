301002922f3cf3b56b9a35897653e03a876979b9
Sagun Shrestha <sagun.shrestha@braintip.ai>
2024-11-13 20:19:04 +0545

    sdfno

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "build"), // Use 'build' directory for GitHub Pages
     filename: "bundle.js",
-    publicPath: "/css_demonstration/", // Set to your GitHub repo name
+    publicPath: "/css_demonstration", // Set to your GitHub repo name
   },
   devServer: {
     port: 3010,
